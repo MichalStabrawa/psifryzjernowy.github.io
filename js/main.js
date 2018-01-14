e3647fabf9338ea4566fb0bacb6e59981725e026
Michal Stabrawa <mistaaqua@gmail.com>
2018-01-14 11:53:19 +0100

    changes
    New header and color website, new modal and new background img

--- js/main.js
+++ js/main.js
@@ -108,43 +108,18 @@ document.getElementById('priceL').addEventListener('click', priceShowLarge);
 
 
 
-/*LIGHBOX EXAMPLE script*****************************************************************************************************************************galeria.html*/
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
-}
 
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
-}
 
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+$(document).ready(function(){
+    $('.close').click(function(){
+        $('.modal-contact').hide(1000);
+    });
+    $('#btn-show').click(function(){
+        $('.modal-contact').show(1000);
+    })
+})
 
 
 
